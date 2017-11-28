f59ce49e4ac27d2ac16b37b45c46b378d98c892a
J. B. Rainsberger <me@jbrains.ca>
2017-11-28 09:55:56 +0100

    We now display a clearer message when the barcode is empty.

--- src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -34,6 +34,17 @@ public class SellOneItemTest {
         Assert.assertEquals("Product not found for 99999", display.getText());
     }
 
+    @Test
+    public void emptyBarcode() throws Exception {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
+
+        sale.onBarcode("");
+
+        Assert.assertEquals("Scanning error: empty barcode", display.getText());
+
+    }
+
     public static class Sale {
         private Display display;
 
@@ -42,7 +53,9 @@ public class SellOneItemTest {
         }
 
         public void onBarcode(final String barcode) {
-            if ("12345".equals(barcode))
+            if ("".equals(barcode))
+                display.setText("Scanning error: empty barcode");
+            else if ("12345".equals(barcode))
                 display.setText("€ 5.50");
             else if ("23456".equals(barcode))
                 display.setText("€ 7.95");
