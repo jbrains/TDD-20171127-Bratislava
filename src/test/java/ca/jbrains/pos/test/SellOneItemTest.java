69c885a567b154e26213cae7ef0d03b907ac3376
J. B. Rainsberger <me@jbrains.ca>
2017-11-28 09:53:02 +0100

    We gracefully handle not finding the product.

--- src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
+++ src/test/java/ca/jbrains/pos/test/SellOneItemTest.java
@@ -24,6 +24,16 @@ public class SellOneItemTest {
         Assert.assertEquals("€ 7.95", display.getText());
     }
 
+    @Test
+    public void productNotFound() throws Exception {
+        final Display display = new Display();
+        final Sale sale = new Sale(display);
+
+        sale.onBarcode("99999");
+
+        Assert.assertEquals("Product not found for 99999", display.getText());
+    }
+
     public static class Sale {
         private Display display;
 
@@ -34,8 +44,10 @@ public class SellOneItemTest {
         public void onBarcode(final String barcode) {
             if ("12345".equals(barcode))
                 display.setText("€ 5.50");
-            else
+            else if ("23456".equals(barcode))
                 display.setText("€ 7.95");
+            else
+                display.setText(String.format("Product not found for %s", barcode));
         }
     }
 
